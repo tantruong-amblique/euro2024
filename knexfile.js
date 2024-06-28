a01dc094cb2fdccbe6b1340a3e9d5f6fd202ff6a
Tan Truong <42831000+tantruong-amblique@users.noreply.github.com>
2024-06-28 11:51:20 +0700

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,18 +1,19 @@
 module.exports = {
     development: {        
-        client: "pg",
-        connection: {
-            connectionString: process.env.DATABASE_URL,
-            host: process.env.PGHOST,
-            port: process.env.PGPORT,
-            database: process.env.PGDATABASE,
-            user: process.env.PGUSER,
-            password: process.env.PGPASSWORD,
-            sslmode: 'require',
-            ssl: true,
-            application_name: "knex-development"
-        },
-        
+        // client: "pg",
+        // connection: {
+        //     connectionString: process.env.DATABASE_URL,
+        //     host: process.env.PGHOST,
+        //     port: process.env.PGPORT,
+        //     database: process.env.PGDATABASE,
+        //     user: process.env.PGUSER,
+        //     password: process.env.PGPASSWORD,
+        //     sslmode: 'require',
+        //     ssl: true,
+        //     application_name: "knex-development"
+        // },
+        client: 'pg',
+        connection: process.env.DATABASE_URL,
         pool: {
             min: 0,
             max: 10,
