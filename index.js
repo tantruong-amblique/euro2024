d36c8341a97d233a71dfb3e3ec26aa908028dcb8
Tan Truong <42831000+tantruong-amblique@users.noreply.github.com>
2024-06-28 12:52:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const { knex } = require("./db");
 const i18next = require("i18next");
 const middleware = require("i18next-http-middleware");
 
-console.log(knex.client.config);
+// console.log(knex.client.config);
 
 i18next.use(middleware.LanguageDetector).init({
     fallbackLng: "vi",
